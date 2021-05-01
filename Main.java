30484d4bc82aeb738068fa81fe34485215925a37
Mike Stagney <49824414+mikestagney@users.noreply.github.com>
2021-04-30 23:50:51 -0400

    remove comments

--- Main.java
+++ Main.java
@@ -4,26 +4,15 @@ package life;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        //UserInput input = new UserInput();
+        
 
         Board board = new Board();
         GameOfLife window = new GameOfLife(board); //took out board
-        Generation generate = new Generation(window, board);
-        //Generation.generates(window, board);
-
-        //Thread t1 = new Thread(() -> new GameOfLife());
-
-        //t1.start();
-        //t1.sleep(50000);
-
+       
+        
     }
 
 
-        //Board grid = new Board(input.N);
-
-
-        //Generation.generates(15, grid);
-
-        //Output.printToConsole(grid);
+        
     }
 
